4bdaa5d1d5c2f077ff318cb0ba43d6ffe78add03
Swaraj <swarajmali072004@gmail.com>
2024-03-30 02:34:34 +0530

    Commit

--- src/Components/Banner.tsx
+++ src/Components/Banner.tsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React, { SetStateAction, useState } from 'react'
 import { Input } from './ui/input'
 import { Button } from './ui/button'
 import { cn } from '@/lib/utils'
 
 export default function Banner() {
+
+const[query , setquery] = useState("")
+
+const onsetquery =(e:React.ChangeEvent<HTMLInputElement>)=>{
+
+    setquery(e.target.value)
+}
+
+console.log(query)
+
   return (
-    <div className={cn("    ml-48 mr-48 mt-20  text-left")}>
+    <div className={cn("    ml-48 mr-48 mt-20  text-left max-lg:ml-16 max-lg:mr-16")}>
       
 
 
@@ -21,11 +31,11 @@ Thousands of jobs in computer , engineering and technology sectors are waiting f
 
 
 
-<div className={cn("flex gap-x-1 mt-8")}>
+<div className={cn("flex gap-x-1 mt-8 max-md:flex-col max-md:gap-y-3")}>
 
-<Input className={cn(" w-[800px]")}  placeholder='What position are you looking for?' />
-<Input className={cn("w-[600px]")} placeholder='Location'/>
-<Button className={cn(" w-44")} >Search</Button>
+<Input className={cn(" w-[800px] max-md:w-auto")}  value={query} onChange={onsetquery} placeholder='What position are you looking for?' />
+<Input className={cn("w-[600px] max-md:w-auto")} placeholder='Location'/>
+<Button className={cn(" w-44 max-md:w-auto")} >Search</Button>
 </div>
 
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,27 @@
-import Banner from '@/Components/Banner'
-import LandingPage from '@/Components/LandingPage'
-import React from 'react'
+"use client";
+import Banner from "@/Components/Banner";
+import LandingPage from "@/Components/LandingPage";
+import React, { useEffect, useState } from "react";
 
 export default function Home() {
+  const [Jobs, SetJobs] = useState([]);
+
+  useEffect(() => {
+    const getdata = async () => {
+      const data = await fetch("jobs.json");
+      const parsedata = await data.json();
+      console.log(parsedata);
+      SetJobs(parsedata);
+    };
+    getdata()
+  }, []);
+
+  console.log("erf");
+
   return (
     <>
-    {/* <LandingPage/> */}
-    <Banner/>
+      {/* <LandingPage/> */}
+      <Banner />
     </>
-  )
+  );
 }
