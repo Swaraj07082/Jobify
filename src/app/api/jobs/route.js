e3a6b440360966f1574f83456fcc8fd0d40ceb69
Swaraj <swarajmali072004@gmail.com>
2024-04-13 00:13:54 +0530

    jobs endpoint created

--- src/app/api/jobs/route.js
+++ src/app/api/jobs/route.js
@@ -1,8 +1,16 @@
-import {NextResponse} from 'next/server'
-export const GET = async(req, res)=>{
+import { NextResponse } from "next/server";
+import db from '../../../lib/db'
+export const GET = async (req, res) => {
+  try {
 
-return new NextResponse(
-    JSON.stringify({ message: "Something went wrong!" }, { status: 500 })
-);
+const jobs = await db.job.findMany();
 
-}
+return new NextResponse(JSON.stringify(jobs , {status : 200}))
+
+
+  } catch (error) {
+    return new NextResponse(
+      JSON.stringify({ message: "Something went wrong!" }, { status: 500 })
+    );
+  }
+};
