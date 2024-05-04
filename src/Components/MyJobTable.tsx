58152fc1e1816069156bca659c6b269dc0c951b3
Swaraj <swarajmali072004@gmail.com>
2024-05-05 05:03:29 +0530

    tablecss

--- src/Components/MyJobTable.tsx
+++ src/Components/MyJobTable.tsx
@@ -1,3 +1,4 @@
+'use client'
 import {
   Table,
   TableBody,
@@ -8,6 +9,7 @@ import {
   TableHeader,
   TableRow,
 } from "../Components/ui/table";
+import { Button } from "./ui/button";
 
 const invoices = [
   {
@@ -15,61 +17,60 @@ const invoices = [
     TITLE: "Paid",
     COMPANYNAME: "$250.00",
     SALARY: "Credit Card",
+    EDIT:<Button onClick={()=>{console.log("clicked")}} >Edit</Button>,
+    DELETE:<Button>Delete</Button>
+
   },
   {
     NO: "INV001",
     TITLE: "Paid",
     COMPANYNAME: "$250.00",
     SALARY: "Credit Card",
+    EDIT:<Button>Edit</Button>,
+    DELETE:<Button>Delete</Button>
   },
   {
     NO: "INV001",
     TITLE: "Paid",
     COMPANYNAME: "$250.00",
     SALARY: "Credit Card",
+    EDIT:<Button>Edit</Button>,
+    DELETE:<Button>Delete</Button>
+
   },
   {
     NO: "INV001",
     TITLE: "Paid",
     COMPANYNAME: "$250.00",
     SALARY: "Credit Card",
+    EDIT:<Button>Edit</Button>,
+    DELETE:<Button>Delete</Button>
+
   },
   {
     NO: "INV001",
     TITLE: "Paid",
     COMPANYNAME: "$250.00",
     SALARY: "Credit Card",
-  },
-  {
-    NO: "INV001",
-    TITLE: "Paid",
-    COMPANYNAME: "$250.00",
-    SALARY: "Credit Card",
-  },
-  {
-    NO: "INV001",
-    TITLE: "Paid",
-    COMPANYNAME: "$250.00",
-    SALARY: "Credit Card",
-  },
-  {
-    NO: "INV001",
-    TITLE: "Paid",
-    COMPANYNAME: "$250.00",
-    SALARY: "Credit Card",
-  },
+    EDIT:<Button>Edit</Button>,
+    DELETE:<Button>Delete</Button>
+
+  }
 ];
 
 export function MyJobTable() {
   return (
-    <Table>
-      <TableCaption>A list of your recent invoices.</TableCaption>
+    <Table className=" w-[800px]">
+      {/* <TableCaption>A list of your recent invoices.</TableCaption> */}
       <TableHeader>
         <TableRow>
-          <TableHead className="w-[100px] text-center">NO.</TableHead>
-          <TableHead className="text-center">TITLE</TableHead>
-          <TableHead className="text-center">COMPANY NAME</TableHead>
-          <TableHead className="text-center">SALARY</TableHead>
+          <TableHead className="w-32 text-center">NO.</TableHead>
+          <TableHead className=" w-32 text-center">TITLE</TableHead>
+          <TableHead className=" w-64 text-center">COMPANY NAME</TableHead>
+          <TableHead className=" w-32 text-center">SALARY</TableHead>
+          <TableHead className=" w-32 text-center">EDIT</TableHead>
+          <TableHead className="w-32 text-center">DELETE</TableHead>
+
         </TableRow>
       </TableHeader>
       <TableBody>
@@ -78,16 +79,19 @@ export function MyJobTable() {
             <TableCell className="font-medium text-center">{invoice.NO}</TableCell>
             <TableCell className="text-center">{invoice.TITLE}</TableCell>
             <TableCell className="text-center">{invoice.COMPANYNAME}</TableCell>
-            <TableCell className=" text-center">{invoice.SALARY}</TableCell>
-          </TableRow>
+            <TableCell className=" text-center" >{invoice.SALARY}</TableCell>
+            <TableCell className=" text-center">{invoice.EDIT}</TableCell>
+            <TableCell className=" text-center">{invoice.DELETE}</TableCell>
+
+        </TableRow>
         ))}
       </TableBody>
-      <TableFooter>
+      {/* <TableFooter>
         <TableRow>
-          {/* <TableCell colSpan={3}>Total</TableCell>
-          <TableCell className="text-right">$2,500.00</TableCell> */}
+          <TableCell colSpan={3}>Total</TableCell>
+          <TableCell className="text-right">$2,500.00</TableCell>
         </TableRow>
-      </TableFooter>
+      </TableFooter> */}
     </Table>
   );
 }
