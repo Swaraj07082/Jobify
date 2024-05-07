73df181da7181bfc9cff06cf34387dd8bf7f5e6d
Swaraj <swarajmali072004@gmail.com>
2024-05-07 23:46:22 +0530

    table css

--- src/app/my-jobs/page.js
+++ src/app/my-jobs/page.js
@@ -1,23 +1,25 @@
-import { LoginForm } from '@/Components/LoginForm'
-import React from 'react'
-import {Input} from '../../Components/ui/input'
-import {Card} from '../../Components/ui/card'
-import { MyJobTable } from '@/Components/MyJobTable'
+import { LoginForm } from "@/Components/LoginForm";
+import React from "react";
+import { Input } from "../../Components/ui/input";
+import { Card } from "../../Components/ui/card";
+import { MyJobTable } from "@/Components/MyJobTable";
+import { Button } from "../../Components/ui/button";
 
 export default function page() {
   return (
-<>
+    <>
+      <div className=" flex flex-col justify-center items-center">
+        <div className=" flex gap-8">
+          <Input style={{ width: "700px" }} />
+          <Button>Search</Button>
+        </div>
 
- <div className=' flex flex-col justify-center items-center'>
-
-<Input className=' py-10'/>
-
-<Card className=' w-fit' >
-<MyJobTable/>
-</Card>
-
-
-</div>
-</>
-  )
+        <div>
+          <Card className="w-fit">
+            <MyJobTable />
+          </Card>
+        </div>
+      </div>
+    </>
+  );
 }
