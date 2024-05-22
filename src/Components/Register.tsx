6d6e8a138b93c2256b03bde3149bcb817caa97e6
Swaraj <swarajmali072004@gmail.com>
2024-05-23 03:07:38 +0530

    register auth

--- src/Components/Register.tsx
+++ src/Components/Register.tsx
@@ -1,64 +1,180 @@
-import Link from "next/link"
+"use client";
+import Link from "next/link";
 
-import { Button } from "../Components/ui/button"
+import { Button } from "../Components/ui/button";
+import { z } from "zod";
 import {
   Card,
   CardContent,
   CardDescription,
   CardHeader,
   CardTitle,
-} from "../Components/ui/card"
-import { Input } from "@/Components/ui/input"
-import { Label } from "@/Components/ui/label"
+} from "../Components/ui/card";
+import { Input } from "@/Components/ui/input";
+import { Label } from "@/Components/ui/label";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../Components/ui/form";
+
+const formSchema = z.object({
+  username: z
+    .string()
+    .refine((value) => value.length > 0, {
+      message: "Please enter your username",
+    })
+    .refine((value) => value.length >= 2, {
+      message: "Minimum 2 characters required",
+    }),
+  email: z.string().email().min(1, {
+    message: "This field has to be filled.",
+  }),
+  password: z
+    .string()
+    .min(8, { message: "Password must be at least 8 characters long" })
+    .max(32, { message: "Password must be at most 32 characters long" })
+    .refine((value) => /[A-Z]/.test(value), {
+      message: "Password must contain at least one uppercase letter",
+    })
+    .refine((value) => /[a-z]/.test(value), {
+      message: "Password must contain at least one lowercase letter",
+    })
+    .refine((value) => /[0-9]/.test(value), {
+      message: "Password must contain at least one number",
+    })
+    .refine((value) => /[^A-Za-z0-9]/.test(value), {
+      message: "Password must contain at least one special character",
+    }),
+});
 
 export function Register() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+      email: "",
+      password: "",
+    },
+  });
+  const { reset } = form;
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+    alert('Submitted')
+    reset()
+
+  }
+
   return (
-    <Card className="mx-auto max-w-sm">
-      <CardHeader>
-        <CardTitle className="text-xl">Register</CardTitle>
-        <CardDescription>
-          Enter your information to create an account
-        </CardDescription>
-      </CardHeader>
-      <CardContent>
-        <div className="grid gap-4">
-          {/* <div className="grid grid-cols-2 gap-4"> */}
-            <div className="grid gap-2">
-              <Label htmlFor="username">Username</Label>
-              <Input id="username" placeholder="Max" required />
-            </div>
-            {/* <div className="grid gap-2">
+    <>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <Card className="mx-auto max-w-sm">
+            <CardHeader>
+              <CardTitle className="text-xl">Register</CardTitle>
+              <CardDescription>
+                Enter your information to create an account
+              </CardDescription>
+            </CardHeader>
+            <CardContent>
+              <div className="grid gap-4">
+                {/* <div className="grid grid-cols-2 gap-4"> */}
+                <div className="grid gap-2">
+                  <FormField
+                    control={form.control}
+                    name="username"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Username</FormLabel>
+                        <FormControl>
+                          <Input placeholder="Username..." {...field} />
+                        </FormControl>
+                        {/* <FormDescription>
+                          This is your public display name.
+                        </FormDescription> */}
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  {/* <Label htmlFor="username">Username</Label> */}
+                  {/* <Input id="username" placeholder="Max" required /> */}
+                </div>
+                {/* <div className="grid gap-2">
               <Label htmlFor="last-name">Last name</Label>
               <Input id="last-name" placeholder="Robinson" required />
             </div> */}
-          {/* </div> */}
-          <div className="grid gap-2">
-            <Label htmlFor="email">Email</Label>
-            <Input
-              id="email"
-              type="email"
-              placeholder="m@example.com"
-              required
-            />
-          </div>
-          <div className="grid gap-2">
-            <Label htmlFor="password">Password</Label>
-            <Input id="password" type="password" />
-          </div>
-          <Button type="submit" className="w-full">
-            Create an account
-          </Button>
-          {/* <Button variant="outline" className="w-full">
+                {/* </div> */}
+                <div className="grid gap-2">
+                  <FormField
+                    control={form.control}
+                    name="email"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Email</FormLabel>
+                        <FormControl>
+                          <Input placeholder="Email..." {...field} />
+                        </FormControl>
+                        {/* <FormDescription>
+                          This is your public  name.
+                        </FormDescription> */}
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  {/* <Label htmlFor="email">Email</Label>
+              <Input
+                id="email"
+                type="email"
+                placeholder="m@example.com"
+                required
+                /> */}
+                </div>
+                <div className="grid gap-2">
+                  {/* <Label htmlFor="password">Password</Label>
+                  <Input id="password" type="password" /> */}
+                  <FormField
+                    control={form.control}
+                    name="password"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Password</FormLabel>
+                        <FormControl>
+                          <Input placeholder="password..." {...field} type="password"/>
+                        </FormControl>
+                        {/* <FormDescription>
+                          This is your public  name.
+                        </FormDescription> */}
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                </div>
+                <Button type="submit" className="w-full">
+                  Create an account
+                </Button>
+                {/* <Button variant="outline" className="w-full">
             Sign up with GitHub
           </Button> */}
-        </div>
-        <div className="mt-4 text-center text-sm">
-          Already have an account?{" "}
-          <Link href="/login" className="underline">
-            Login
-          </Link>
-        </div>
-      </CardContent>
-    </Card>
-  )
+              </div>
+              <div className="mt-4 text-center text-sm">
+                Already have an account?{" "}
+                <Link href="/login" className="underline">
+                  Login
+                </Link>
+              </div>
+            </CardContent>
+          </Card>
+        </form>
+      </Form>
+    </>
+  );
 }
--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -1,12 +1,108 @@
-import { NextResponse } from "next/server";
+import db from "@/lib/db";
+import { Hash } from "crypto";
+import { NextApiRequest } from "next";
+import { NextRequest, NextResponse } from "next/server";
+import { hash } from "bcrypt";
+import { z } from "zod";
 
 // POST and GET name matters
+// every type of request like get post has a body headers etc
+// In HTTP, the request body is part of the request message carrying data from the client to the server. It's crucial for methods like POST, PUT, and PATCH, used to create, update, or modify resources. For example, in a POST request to create a user account, user details are in the request body
 
-export async function GET(req: Request) {
+
+
+const formSchema = z.object({
+  username: z
+    .string()
+    .refine((value) => value.length > 0, {
+      message: "Please enter your username",
+    })
+    .refine((value) => value.length >= 2, {
+      message: "Minimum 2 characters required",
+    }),
+  email: z.string().email().min(1, {
+    message: "This field has to be filled.",
+  }),
+  password: z
+    .string()
+    .min(8, { message: "Password must be at least 8 characters long" })
+    .max(32, { message: "Password must be at most 32 characters long" })
+    .refine((value) => /[A-Z]/.test(value), {
+      message: "Password must contain at least one uppercase letter",
+    })
+    .refine((value) => /[a-z]/.test(value), {
+      message: "Password must contain at least one lowercase letter",
+    })
+    .refine((value) => /[0-9]/.test(value), {
+      message: "Password must contain at least one number",
+    })
+    .refine((value) => /[^A-Za-z0-9]/.test(value), {
+      message: "Password must contain at least one special character",
+    }),
+});
+
+
+
+
+export const POST = async (req: Request) => {
   try {
+    // const idk = await db.job.findMany();
     const body = await req.json();
-    // console.log(body)
 
-    return NextResponse.json(body);
-  } catch (error) {}
-}
+    const { username, email, password } = formSchema.parse(body) ;
+
+    const existingUserByEmail = await db.user.findUnique({
+      where: { email: email },
+    });
+
+    if (existingUserByEmail) {
+      return NextResponse.json(
+        {
+          user: null,
+          message: "User with this Email Already Exists",
+        },
+        {
+          status: 409,
+        }
+      );
+    }
+
+    const existingUserByUsername = await db.user.findUnique({
+      where: { username: username },
+    });
+
+    if (existingUserByUsername) {
+      return NextResponse.json(
+        {
+          user: null,
+          message: "User with this Username Already Exists",
+        },
+        {
+          status: 409,
+        }
+      );
+    }
+
+    const hashedpassword = await hash(password, 10);
+
+    const newUser = await db.user.create({
+      data: {
+        username: username,
+        email: email,
+        password: hashedpassword,
+      },
+    });
+
+
+    const {password : newUserpassowrd , ...rest} = newUser;
+    return new NextResponse(
+      JSON.stringify({ user: rest, message: "User created successfully" }),
+      { status: 200 }
+    );
+  } catch (error) {
+    return new NextResponse(
+      JSON.stringify({ message: "Something went wrong!" }),
+      { status: 400 }
+    );
+  }
+};
