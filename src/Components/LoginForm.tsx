28a4b989a4b4eed12f9d223b88bdf1ca4579556c
Swaraj <swarajmali072004@gmail.com>
2024-05-24 02:14:45 +0530

    Login Backend

--- src/Components/LoginForm.tsx
+++ src/Components/LoginForm.tsx
@@ -1,4 +1,5 @@
-import { Button } from "./ui/button"
+"use client";
+import { Button } from "./ui/button";
 import {
   Card,
   CardContent,
@@ -6,32 +7,154 @@ import {
   CardFooter,
   CardHeader,
   CardTitle,
-} from "./ui/card"
-import { Input } from "./ui/input"
-import { Label } from "./ui/label"
+} from "./ui/card";
+import { Input } from "./ui/input";
+import { Label } from "./ui/label";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { FaEye } from "react-icons/fa";
+import { FaEyeSlash } from "react-icons/fa";
+
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../Components/ui/form";
+import { useState } from "react";
+
+import {signIn} from 'next-auth/react'
+
+const formSchema = z.object({
+  email: z.string().email().min(1, {
+    message: "This field has to be filled.",
+  }),
+  password: z.string().min(1, {
+    message: "This field has to be filled.",
+  }),
+});
 
 export default function LoginForm() {
+  const [ViewPass, SetViewPass] = useState<Boolean>(true);
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+  const { reset } = form;
+
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+
+    const LoginData = await signIn('credentials' ,{
+      email : values.email ,
+      password : values.password,
+      redirect : false
+    })
+
+    console.log(LoginData)
+  }
+
   return (
-    <Card className="w-full max-w-sm">
-      <CardHeader>
-        <CardTitle className="text-2xl">Login</CardTitle>
-        <CardDescription>
-          Enter your email below to login to your account.
-        </CardDescription>
-      </CardHeader>
-      <CardContent className="grid gap-4">
-        <div className="grid gap-2">
-          <Label htmlFor="email">Email</Label>
-          <Input id="email" type="email" placeholder="m@example.com" required />
-        </div>
-        <div className="grid gap-2">
-          <Label htmlFor="password">Password</Label>
-          <Input id="password" type="password" required />
-        </div>
-      </CardContent>
-      <CardFooter>
-        <Button className="w-full" type="submit">Sign in</Button>
-      </CardFooter>
-    </Card>
-  )
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <Card className="w-full max-w-sm">
+          <CardHeader>
+            <CardTitle className="text-2xl">Login</CardTitle>
+            <CardDescription>
+              Enter your email below to login to your account.
+            </CardDescription>
+          </CardHeader>
+          <CardContent className="grid gap-4">
+            <div className="grid gap-2">
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input
+                        id="email"
+                        type="email"
+                        placeholder="m@example.com"
+                        {...field}
+                      />
+                    </FormControl>
+                    {/* <FormDescription>
+                This is your public display name.
+              </FormDescription> */}
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              {/* <Label htmlFor="email">Email</Label>
+              <Input
+                id="email"
+                type="email"
+                placeholder="m@example.com"
+                required
+              /> */}
+            </div>
+            <div className="grid gap-2">
+              {/* <Label htmlFor="password">Password</Label>
+              <Input id="password" type="password" required /> */}
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password</FormLabel>
+                    <div className="flex items-center ">
+                      {ViewPass ? (
+                        <FaEye
+                          onClick={() => {
+                            SetViewPass(!ViewPass);
+                          }}
+                          className=" absolute right-[40%] "
+                        />
+                      ) : (
+                        <FaEyeSlash
+                          onClick={() => {
+                            SetViewPass(!ViewPass);
+                          }}
+                          className=" absolute right-[40%] "
+                        />
+                      )}
+
+                      <FormControl>
+                        <Input
+                          placeholder="password..."
+                          {...field}
+                          type={ViewPass ? "password" : "text"}
+                        />
+                      </FormControl>
+                    </div>
+                    {/* <FormDescription>
+                This is your public display name.
+              </FormDescription> */}
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+          </CardContent>
+          <CardFooter>
+            <Button className="w-full" type="submit">
+              Log in
+            </Button>
+          </CardFooter>
+        </Card>
+      </form>
+    </Form>
+  );
 }
---
+++ src/lib/auth.ts
@@ -0,0 +1,71 @@
+import { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import PrismaAdapter from "@next-auth/prisma-adapter";
+import db from "./db";
+import { compare } from "bcrypt";
+
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter({ prisma: db }),
+  session: {
+    strategy: "jwt",
+  },
+  pages: {
+    signIn: "/login",
+  },
+  providers: [
+    CredentialsProvider({
+      // The name to display on the sign in form (e.g. 'Sign in with...')
+      name: "Credentials",
+      // The credentials is used to generate a suitable form on the sign in page.
+      // You can specify whatever fields you are expecting to be submitted.
+      // e.g. domain, username, password, 2FA token, etc.
+      // You can pass any HTML attribute to the <input> tag through the object.
+      credentials: {
+        email: { label: "Email", type: "email", placeholder: "jsmith" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        // You need to provide your own logic here that takes the credentials
+        // submitted and returns either a object representing a user or value
+        // that is false/null if the credentials are invalid.
+        // e.g. return { id: 1, name: 'J Smith', email: 'jsmith@example.com' }
+        // You can also use the `req` object to obtain additional parameters
+        // (i.e., the request IP address)
+        // const res = await fetch("/your/endpoint", {
+        //   method: "POST",
+        //   body: JSON.stringify(credentials),
+        //   headers: { "Content-Type": "application/json" },
+        // });
+        // const user = await res.json();
+
+        // If no error and we have user data, return it
+        if (!credentials?.email || !credentials?.password) {
+          return null;
+        }
+        // Return null if user data could not be retrieved
+        const existingUser = await db.user.findUnique({
+          where: { email: credentials?.email },
+        });
+
+        if (!existingUser) {
+          return null;
+        }
+
+        const passwordMatch = await compare(
+          credentials.password,
+          existingUser.password
+        );
+
+        if (!passwordMatch) {
+          return null;
+        }
+
+        return {
+          id: `${existingUser.id}`,
+          username: existingUser.username,
+          email: existingUser.email,
+        };
+      },
+    }),
+  ],
+};
